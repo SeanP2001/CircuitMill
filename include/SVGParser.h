704816de41676f8894baaf60e997ba8a63f194b0
Sean <stprice01@outlook.com>
2023-10-06 17:07:30 +0100

    V0.3.0 Implemented parseRectangle Function

--- include/SVGParser.h
+++ include/SVGParser.h
@@ -1,6 +1,6 @@
 
-#ifndef _SVG_PARSER_
-#define _SVG_PARSER_
+#ifndef _SVG_PARSER_H_
+#define _SVG_PARSER_H_
 
 #include <iostream>
 #include <fstream>
@@ -15,13 +15,19 @@ class SVGParser
     private:
         std::ifstream file;
 
+        int getwidth(const std::string& svgElement);
+
     public:
         SVGParser();
         SVGParser(const std::string& filePath);
 
         std::vector<Shape> parse();
+        std::vector<Shape> parse(const std::string& svgElement);
 
-        Rectangle parseRectangle();
+        Rectangle parseRectangle(const std::string& rectangleElement);
+
+        std::string getSVGAttribute(const std::string attributeName, const std::string& rectangleElement);
+        double getSVGDoubleAttribute(const std::string attributeName, const std::string& rectangleElement);
 };
 
-#endif // _SVG_PARSER_
+#endif // _SVG_PARSER_H_
--- src/SVGParser.cpp
+++ src/SVGParser.cpp
@@ -1,5 +1,46 @@
+#include "SVGParser.h"
 
-class SVGParser
+SVGParser::SVGParser()
 {
-    
-};
+
+}
+
+SVGParser::SVGParser(const std::string& filePath)
+{
+
+}
+
+std::vector<Shape> SVGParser::parse()
+{
+
+}
+
+std::vector<Shape> SVGParser::parse()
+{
+
+}
+
+std::vector<Shape> SVGParser::parse(const std::string& svgElement)
+{
+
+}
+
+Rectangle SVGParser::parseRectangle(const std::string& rectangleElement)
+{
+    return Rectangle(getSVGDoubleAttribute("x", rectangleElement), getSVGDoubleAttribute("y", rectangleElement), getSVGDoubleAttribute("width", rectangleElement), getSVGDoubleAttribute("height", rectangleElement));
+}
+
+std::string SVGParser::getSVGAttribute(const std::string attributeName, const std::string& svgElement)
+{
+    std::string attribute = attributeName + "=\"";
+
+    size_t posBefore = svgElement.find(attribute) + attribute.length();
+    size_t posAfter = svgElement.find("\"", posBefore);
+
+    return svgElement.substr(posBefore, posAfter);
+}
+
+double SVGParser::getSVGDoubleAttribute(const std::string attributeName, const std::string& svgElement)
+{
+    return std::stod(getSVGAttribute(attributeName, svgElement));
+}
